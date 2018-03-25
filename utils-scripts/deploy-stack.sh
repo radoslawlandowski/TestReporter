1ac4b848dce255c7284a9fe79008abf6c383a59f
radoslawlandowski <radoslaw.landowski@gmail.com>
2018-03-25 12:07:05 +0200

    Parametrize script

--- utils-scripts/deploy-stack.sh
+++ utils-scripts/deploy-stack.sh
@@ -7,7 +7,19 @@ stackName="tr-$stackEnv"
 export API_IMAGE_NAME="radoslawlandowski/tr-api:0.1.0"
 export DB_IMAGE_NAME="radoslawlandowski/tr-db:0.1.0"
 export FR_IMAGE_NAME="radoslawlandowski/tr-fr:0.1.0"
-# --------- CONFIGURATION --------- 
+# --------- /CONFIGURATION --------- 
+
+
+# --------- UTIL FUNCTIONS --------- 
+echoTimed() {
+  currentDate=$(date +"%T")
+  echo "$currentDate: $1"
+}
+# --------- /UTIL FUNCTIONS --------- 
+
+# --------- MAIN SCRIPT --------- 
+
+echoTimed "Main script execution started!"
 
 docker pull $API_IMAGE_NAME
 docker pull $DB_IMAGE_NAME
@@ -15,22 +27,24 @@ docker pull $FR_IMAGE_NAME
 
 docker stack deploy -c docker-compose.$stackEnv.yml $stackName
 
-echo "\nThe stack: $stackName has been deployed. Initiating healthchecks...\n"
+echoTimed "The stack: $stackName has been deployed. Initiating healthchecks...\n"
 
 statusCode=""
 healthcheckEndpoint="127.0.0.11:8083/api/test-groups"
 while true; do
-    echo "Checking if service responds with code "200" at $healthcheckEndpoint ..."
+    echoTimed "Checking if service responds with code "200" at $healthcheckEndpoint ..."
     response=$(curl --write-out %{http_code} --silent --output /dev/null $healthcheckEndpoint)
-    echo "Response code: $response"
+    echoTimed "Response code: $response"
     if [ "$response" -eq "200" ]
     then
         break
     fi
-    echo "Non 200 response was obtained, sleeping 3 seconds...\n\n"
+    echoTimed "Non 200 response was obtained, sleeping 3 seconds...\n\n"
     sleep 3
 done
 
-echo "********************** \n***" +
-    "Application $stackName started and working! *** \n" +
-    "***********************"
+echoTimed "\n***************************************
+* Application $stackName started and working! *
+***************************************"
+
+# --------- /MAIN SCRIPT --------- 
