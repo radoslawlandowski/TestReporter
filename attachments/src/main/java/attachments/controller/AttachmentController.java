e73ef46ac3de7d43e56da08e3150b469861ed0f9
Unknown <radoslaw.landowski@gmail.com>
2018-10-06 10:25:10 +0200

    add upload and downlado files

--- attachments/src/main/java/attachments/controller/AttachmentController.java
+++ attachments/src/main/java/attachments/controller/AttachmentController.java
@@ -3,9 +3,13 @@ package attachments.controller;
 import attachments.model.Attachment;
 import attachments.repository.AttachmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 public class AttachmentController {
@@ -13,14 +17,44 @@ public class AttachmentController {
     @Autowired
     AttachmentRepository repository;
 
-    @RequestMapping(value = "/attachments", method = RequestMethod.GET)
-    public Attachment byId() {
-        Attachment a = new Attachment();
+    @RequestMapping(value = "/attachments/{fileId}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<byte[]> byId(@PathVariable("fileId") int fileId) {
+        Attachment a = repository.findById(fileId).get();
 
-        return repository.save(a);
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(MediaType.APPLICATION_OCTET_STREAM_VALUE))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + a.getAttachmentName() + "\"")
+                .body(a.getData());
     }
 
+    @RequestMapping(value = "/attachments",
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_OCTET_STREAM_VALUE,
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Integer> upload(@RequestParam("file") MultipartFile file) throws IOException {
+        Attachment a = new Attachment();
+        a.setAttachmentName(file.getOriginalFilename());
+        a.setData(file.getBytes());
+
+        int id = repository.save(a).getId();
+
+        return ResponseEntity.ok().body(Integer.valueOf(id));
+    }
 }
+
+//
+//    @POST
+//    @Consumes(MediaType.MULTIPART_FORM_DATA)
+//    @Produces(MediaType.APPLICATION_JSON)
+//    @Timed
+//    @UnitOfWork
+//    public Response uploadFile(
+//            @PathParam("testGroupName") String testGroupName,
+//            @FormDataParam("file") InputStream uploadedInputStream,
+//            @FormDataParam("file") FormDataContentDisposition fileDetail) throws Exception {
+
 
 //@Path("/attachments")
 //public class AttachmentController {
--- attachments/src/main/java/attachments/repository/AttachmentRepository.java
+++ attachments/src/main/java/attachments/repository/AttachmentRepository.java
@@ -3,5 +3,5 @@ package attachments.repository;
 import attachments.model.Attachment;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AttachmentRepository extends CrudRepository<Attachment, Long> {
+public interface AttachmentRepository extends CrudRepository<Attachment, Integer> {
 }
