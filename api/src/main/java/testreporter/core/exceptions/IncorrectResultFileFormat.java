4ea0c08ee92076be2749141a022e493f953a6a88
Unknown <radoslaw.landowski@gmail.com>
2018-10-07 13:34:49 +0200

    remove old api

--- api/src/main/java/testreporter/core/exceptions/IncorrectResultFileFormat.java
+++
@@ -1,7 +0,0 @@
-package testreporter.core.exceptions;
-
-public class IncorrectResultFileFormat extends Exception {
-    public IncorrectResultFileFormat(String message) {
-        super(message);
-    }
-}
